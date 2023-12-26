c1a21d4baf59b9daf73b588b3c9a5a394705a5e1
Abdul Majid <majid616365@gmail.com>
2023-12-26 18:08:29 +0530

    update

---
+++ components/Card.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+
+const Card = ({ title, description, image, github, link, techstack }) => {
+    return (
+        <div className=" mx-auto mb-8 bg-white rounded-xl overflow-hidden shadow-md">
+            <div className="relative">
+                <img
+                    className="w-auto mx-auto h-full object-cover object-center"
+                    src={
+                        image ||
+                        "https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
+                    }
+                    alt={title}
+                />
+            </div>
+            <div className="p-6">
+                <h2 className="text-2xl font-bold mb-2">{title}</h2>
+                <p className="text-gray-700 mb-4">{description}</p>
+                <div className="flex gap-x-2">
+                    {techstack && techstack.map((icon) => <div>{icon}</div>)}
+                </div>
+                <div className="flex justify-end">
+                    {github && (
+                        <a
+                            href={github}
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            className="text-blue-500 mr-4 hover:underline"
+                        >
+                            Git
+                        </a>
+                    )}
+                    {link && (
+                        <a
+                            href={link}
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            className="text-blue-500 hover:underline"
+                        >
+                            View Project
+                        </a>
+                    )}
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Card;
--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,8 @@ import utilStyles from "../styles/utils.module.css";
 import Link from "next/link";
 import { getSortedPostsData } from "../lib/posts";
 import Date from "../components/date";
+import Image from "next/image";
+import Card from "../components/Card";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faGithub, faInstagram } from "@fortawesome/free-brands-svg-icons";
@@ -19,17 +21,81 @@ export async function getStaticProps() {
 }
 
 export default function Home({ allPostsData }) {
+
+  const icons = {
+    weavedb: (
+      <Image
+        src="/images/weavedb.png"
+        width={20}
+        height={20}
+        className="pt-1"
+      />
+    ),
+    ipfs: <Image src="/images/ipfs.png" width={20} height={20} />,
+    socketio: <i className="devicon-socketio-original colored text-xl"></i>,
+    golang: <i className="devicon-go-original-wordmark colored text-xl"></i>,
+    git: <i className="devicon-git-plain colored text-xl"></i>,
+    typescript: <i className="devicon-typescript-plain colored text-xl"></i>,
+    firebase: <i className="devicon-firebase-plain colored text-xl"></i>,
+    javascript: <i className="devicon-javascript-plain colored text-xl"></i>,
+    tailwind: <i className="devicon-tailwindcss-plain colored text-xl"></i>,
+    solidity: <i className="devicon-solidity-plain colored text-xl"></i>,
+    golang: <i className="devicon-go-plain colored text-xl"></i>,
+    react: <i className="devicon-react-original colored text-xl"></i>,
+    next: <i className="devicon-nextjs-original colored text-xl"></i>,
+    node: <i className="devicon-nodejs-plain colored text-xl"></i>,
+    express: <i className="devicon-express-original colored text-xl"></i>,
+    vuejs: <i className="devicon-vuejs-plain colored text-xl"></i>,
+    mongodb: <i class="devicon-mongodb-plain colored text-xl"></i>
+  };
+
+
+  const projects = [
+    {
+      name: "Formify Ai",
+      description: "An Ai powered form builder with web3 based validation",
+      image: "/images/formify.png",
+      techstack: [
+        "typescript",
+        "solidity",
+        "next",
+        "node",
+        "tailwind",
+        "weavedb",
+      ].map((tech) => {
+        return icons[tech];
+      }),
+      github: "https://github.com/majid-2002",
+      link: "https://formify-ai.vercel.app/",
+    },
+    {
+      name: "ChessArena",
+      image: "/images/chess.png",
+      description: "A Chess multiplayer game",
+      techstack: ["next", "tailwind", "mongodb", "node", "socketio"].map(
+        (tech) => {
+          return icons[tech];
+        }
+      ),
+      github: "https://github.com/majid-2002/chessarena",
+    },
+  ];
+
   return (
     <Layout home>
       <Head>
         <title>{siteTitle}</title>
+        <link
+          rel="stylesheet"
+          href="https://cdn.jsdelivr.net/gh/devicons/devicon@v2.15.1/devicon.min.css"
+        />
       </Head>
       <section className={utilStyles.headingMd}>
         <p>
           Hello I am <b>Majid</b>, a software developer, working on Next.JS,
           React.JS and Full Stack Apps Using Node.JS
         </p>
-        <div className={utilStyles.socialIconList}>
+        <div className={utilStyles.socialIconList + " pt-4"}>
           <a href="https://www.github.com/majid-2002">
             <FontAwesomeIcon icon={faGithub} size="xl" />
           </a>
@@ -53,6 +119,17 @@ export default function Home({ allPostsData }) {
           ))}
         </ul>
       </section>
+
+      <section>
+        <h2 className={utilStyles.headingLg}>Projects</h2>
+        <div className="pt-4">
+          {projects.map((project) => {
+            return <Card {...project} />;
+          })}
+        </div>
+      </section>
+
+
     </Layout>
   );
 }
